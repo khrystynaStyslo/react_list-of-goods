5aae767e08a334592e7c4dcc6d31567e3b5847d0
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-09-15 08:27:29 +0300

    Updates

---
+++ precache-manifest.7ed85019bf294dec0109ee676f072436.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c335b7a5811096f58aaefba7e76623e2",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "5863e156de3a8f8725fa",
+    "url": "/react_list-of-goods/static/css/main.1fc01807.chunk.css"
+  },
+  {
+    "revision": "c6711347337c0bdbd6ef",
+    "url": "/react_list-of-goods/static/js/2.c0c080c6.chunk.js"
+  },
+  {
+    "revision": "5863e156de3a8f8725fa",
+    "url": "/react_list-of-goods/static/js/main.6a6ec92e.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
